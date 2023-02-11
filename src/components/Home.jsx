bc14f33ab1f20c501302fc4aff1d4dd3392985fd
mrmakr <18046051+mrmakr@users.noreply.github.com>
2023-02-11 13:37:35 +0100

    fetch API

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,20 +3,20 @@ import React from "react";
 const Home = () => {
   return (
     <div className="hero">
-      <div class="card text-bg-dark border-0">
+      <div className="card text-bg-dark border-0">
         <img
           src="https://via.placeholder.com/500"
-          class="card-img"
+          className="card-img"
           alt="..."
           height="500px"
         />
-        <div class="card-img-overlay d-flex flex-column justify-content-center">
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
           <div className="container">
-            <h5 class="card-title display-3 mb-0">Sale</h5>
-            <p class="card-text">
+            <h5 className="card-title display-3 mb-0">Sale</h5>
+            <p className="card-text">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, necessitatibus soluta ratione dicta, blanditiis sit totam deleniti nisi corrupti distinctio laboriosam iusto deserunt perferendis voluptatibus eligendi ab odit eius ullam?
             </p>
-            <p class="card-text">
+            <p className="card-text">
               <small>…</small>
             </p>
           </div>
