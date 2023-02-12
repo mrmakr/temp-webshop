1d26d87c0aa3c129bbc8745cd3a408414d74b087
mrmakr <18046051+mrmakr@users.noreply.github.com>
2023-02-12 23:36:35 +0100

    trying filter/search

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,29 +3,17 @@ import Products from "./Products";
 
 const Home = () => {
   return (
-    <div className="hero">
-      <div className="card text-bg-dark border-0">
-        <img
-          src="https://via.placeholder.com/500"
-          className="card-img"
-          alt="..."
-          height="500px"
-        />
-        <div className="card-img-overlay d-flex flex-column justify-content-center">
-          <div className="container">
-            <h5 className="card-title display-3 mb-0">Sale</h5>
-            <p className="card-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, necessitatibus soluta ratione dicta, blanditiis sit totam deleniti nisi corrupti distinctio laboriosam iusto deserunt perferendis voluptatibus eligendi ab odit eius ullam?
-            </p>
-            <p className="card-text">
-              <small>…</small>
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <Products/>
-    </div>
+    <>
+    <div className="container mb-5 mt-3">
+      <h5>Lorem</h5>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, officiis
+        qui reiciendis eligendi ea repudiandae officia nisi, necessitatibus
+        culpa deleniti quia corporis? Consectetur eum commodi veniam facilis
+        nisi deserunt eius.
+      </p></div>
+      <Products />
+    </>
   );
 };
 
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,80 +1,143 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 const Products = () => {
-          const [loading, setLoading] = useState(false)
-            const [data, setData] = useState(null)
-        
-            useEffect(() => {
-                setLoading(true)
-                axios({
-                  method: "GET",
-                  baseURL: "https://dummyjson.com",
-                  url: "/products?limit=10",
-                })
-                  .then(({ data }) => {
-                    console.log(data.products);
-                    setData(data.products);
-                  })
-                  .catch((err) => console.dir(err))
-                  .finally(() => setLoading(false));
-            }, [])
-        
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
+  // const [filter, setFilter] = useState(data);
+  const [input, setInput] = useState("");
+
+
+// const [allData, setAllData] = useState([]);
+// const [filteredData, setFilteredData] = useState(allData);
+
+
+
+  useEffect(() => {
+    setLoading(true);
+    axios({
+      method: "GET",
+      baseURL: "https://dummyjson.com",
+      url: "/products?limit=30",
+    })
+      .then(({ data }) => {
+        console.log(data.products);
+        setData(data.products);
+        // setAllData(data.products);
+        // setFilteredData(data.products);
+      })
+      .catch((err) => console.dir(err))
+      .finally(() => setLoading(false));
+  }, []);
+
+//  const handleSearch = (event) => {
+//    let value = event.target.value.toLowerCase();
+//    let result = [];
+//    console.log(value);
+//    result = allData.filter((data) => {
+//      return data.title.search(value) !== -1;
+//    });
+//    setFilteredData(result);
+//  };
+
+
+
+
+
+
+
+  // const filterProduct = (cat) => {
+  //   const updatedList = data.filter((x) => x.category === cat);
+  //   setFilter(updatedList);
+  // };
+
+
+
+
+
+
+
+const info = data
+  .filter((product) =>
+    product.name.toLowerCase().includes(input.toLowerCase())
+  )
+  .map((product, index) => <div key={index}>{product.name}</div>);
+
+const handleChange = (e) => {
+  setInput(e.target.value);
+};
+
+  return (
+    <div class="container">
+      <div className="buttons">
+        <input type="text" onChange={handleChange} />
+        <div>{info}</div>
+        {/* <input type="text" onChange={(event) => handleSearch(event)} /> */}
+
+        {/* <button
+          className="btn btn-outline-dark"
+          onClick={() => setFilter(data)}
+        >
+          All
+        </button>
+        <button
+          className="btn btn-outline-dark"
+          onClick={() => filterProduct("laptops")}
+        >
+          laptop
+        </button> */}
+      </div>
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        {loading && "Loading..."}
+        {!!data && data.length > 0 ? (
+          data.map((product) => {
             return (
-              <div class="container">
-                <div class="row row-cols-1 row-cols-md-3 g-4">
-                  <h1>Fake Shop API response:</h1>
-                  {loading && "Loading..."}
-                  {!!data && data.length > 0 ? (
-                    data.map((product) => {
-                      return (
-                        <>
-                          <div class="col" key={product.name}>
-                            {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop  */}
-                            <div className="card h-100" key={product.id}>
-                              <img
-                                src={product.thumbnail}
-                                className="card-img-top"
-                                alt="..."
-                              />
-                              <div className="card-body">
-                                <h5 className="card-title">{product.name}</h5>
-                                <p className="card-text">
-                                  {product.description}
-                                </p>
-                              </div>
-                              <ul className="list-group list-group-flush">
-                                <li className="list-group-item">
-                                  {product.price}$
-                                </li>
-                                <li className="list-group-item">
-                                  {product.discount}
-                                </li>
-                                <li className="list-group-item">
-                                  {product.rating}
-                                </li>
-                              </ul>
-                              <div className="card-footer">
-                                <a href="#" className="card-link text-muted">
-                                  <small>Card link</small>
-                                </a>
-                                <a href="#" className="card-link text-muted">
-                                 <small>Another link</small> 
-                                </a>
-                              </div>
-                            </div>
-                          </div>
-                        </>
-                      );
-                    })
-                  ) : (
-                    <p>API did not provided any product, try again.</p>
-                  )}
+              <>
+                <div class="col" key={product.name}>
+                  {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop  */}
+                  <div className="card h-100 border-0" key={product.id}>
+                    <img
+                      src={product.thumbnail}
+                      className="card-img-top"
+                      alt="..."
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{product.title}</h5>
+                      <p className="card-text">{product.description}</p>
+                      <p className="card-text">{product.name}</p>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                      <li className="list-group-item">
+                        <i className="fa fa-usd me-1"></i>
+                        {product.price}$
+                      </li>
+                      <li className="list-group-item">
+                        <i className="fa fa-tags me-1"></i>
+                        {product.discountPercentage}%
+                      </li>
+                      <li className="list-group-item">
+                        <i className="fa fa-star me-1"></i>
+                        {product.rating}
+                      </li>
+                    </ul>
+                    <div className="card-footer text-end">
+                      <a
+                        href="#"
+                        className="text-uppercase card-link text-muted"
+                      >
+                        <small>add to cart</small>
+                      </a>
+                    </div>
+                  </div>
                 </div>
-              </div>
+              </>
             );
-        }
-
-
-
+          })
+        ) : (
+          <p>API did not provided any product, try again.</p>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Products;
